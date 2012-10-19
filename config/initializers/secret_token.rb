66e283b065d515fb9a6c254220ce6e8d9f1bd6df
Naoise <naoise@wikaina.com>
2012-10-20 01:51:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Divebook::Application.config.secret_token = '5fd23e07e0ee728caf5ce4777ea6c235fa4a5c97924674450fbf34e0a0a9a23f0f6b6b254aedbabf790ee8213410079589b632dc2945a6153c36b115654eca8b'
