0dbf35350a563e827292528a2493d7ece178de0c
Sergey Suprunenko <suprunenko.s@gmail.com>
2015-02-11 15:19:51 +0300

    [add tests] Users#index and Users#auth

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,6 +1,40 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController do
+  describe 'GET #index' do
+    let(:divesite) { create(:divesite_with_5_users) }
+    let(:user) { divesite.users.first}
+
+    it 'returns unauthorized' do
+      get :index, divesite_id: divesite.id
+
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    before(:each) do
+      get :index, divesite_id: divesite.id, access_token: user.authentication_token
+    end
+
+    it 'returns 200 OK' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns array of Dive Site users' do
+      expect(json.count).to eq(divesite.users.count)
+    end
+
+    it 'returns only two Dive Site users' do
+      per_page = 2
+
+      get :index,
+          divesite_id: divesite.id,
+          per_page: per_page,
+          access_token: user.authentication_token
+
+      expect(json.count).to eq(per_page)
+    end
+  end
+
   describe 'GET #show' do
     let(:user) { create(:user) }
 
@@ -73,6 +107,30 @@ describe Api::V1::UsersController do
       delete :destroy, id: user.id, access_token: new_user.authentication_token
 
       expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe 'POST #auth' do
+    let(:user) { build(:user) }
+
+    before(:each) do
+      post :auth,
+            name: user.name,
+            email: user.email,
+            password: user.password
+    end
+
+    it 'returns 201 Created' do
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'returns created User' do
+      expect(json['name']).to eq(user.name)
+      expect(json['email']).to eq(user.email)
+    end
+
+    it 'returns access token for the User' do
+      expect(json['access_token']).to be
     end
   end
 end
--- spec/factories/divesites.rb
+++ spec/factories/divesites.rb
@@ -14,5 +14,13 @@ FactoryGirl.define do
   factory :divesite do
     name    Faker::Company.name
     address Faker::Address.city
+
+    factory :divesite_with_5_users do
+      after(:create) do |divesite|
+        5.times do
+          divesite.users << FactoryGirl.create(:user, email: Faker::Internet.email)
+        end
+      end
+    end
   end
 end
